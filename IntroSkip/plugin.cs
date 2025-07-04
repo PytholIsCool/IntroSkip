61022d57acac8bbdfeb21214b5986eb422f9fe23
Pythol <95198306+PytholIsCool@users.noreply.github.com>
2025-07-04 14:39:07 -0500

    Add files via upload

---
+++ IntroSkip/plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using YanAPI.Wrappers;
+using YanAPI.Logging;
+using UnityEngine.SceneManagement;
+
+[BepInPlugin("com.pythol.introskip", "IntroSkip", "1.0.0")]
+[BepInDependency("com.pythol.YanAPI", BepInDependency.DependencyFlags.HardDependency)]
+public class IntroSkip : BaseUnityPlugin {
+    private void Awake() {
+        CLogs.LogInfo("IntroSkip loaded.");
+
+        GameSceneWrapper.OnDisclaimerSceneLoaded += () => {
+            CLogs.LogInfo("Skipping to Main Menu scene...");
+            SceneManager.LoadScene("NewTitleScene", LoadSceneMode.Single);
+        };
+    }
+}
